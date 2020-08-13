60cb9fce5101bdc994cf0a68ca04a9b02ea96c2f
jagadishe88 <jagadishe88@gmail.com>
2020-08-13 18:32:57 -0400

    Added email authentication

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template, url_for, request, redirect
 import csv
 from flask_mail import Mail, Message
+import os
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+# set configuration and instantiate mail
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": 'jagadish.pasham@gmail.com',
+    "MAIL_PASSWORD": 'ytdzhduheugrujco'
+}
+
+app.config.update(mail_settings)
+mail = Mail(app)
 
 @app.route('/')
 def my_home():
@@ -16,30 +29,20 @@ def html_page(page_name):
     return render_template(page_name)
 
 
-def write_to_file(data):
-    with open('database.txt', 'a') as db:
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
-        file = db.write(f'\n{email}, {subject}, {message}')
-
-
-def write_to_csv(data):
-    with open('db.csv', newline='', mode='a') as db2:
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
-        csv_wiriter = csv.writer(db2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_wiriter.writerow([email, subject, message])
-
-
-@app.route('/thankyou', methods=['POST'])
+@app.route('/thankyou', methods=['POST', 'GET'])
 def thankyou():
-    return redirect('thankyou.html')
+    if request.method == 'POST':
+        form = request.form
+        emailto = form['email']
+        subject = form['subject']
+        message = form['message']
 
 
+
+        msg = Message(subject, recipients = "me@jenkarla.com" , sender= [emailto] )
+        mail.send(msg)
+        return redirect('home.html')
 
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
-
