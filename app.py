69075ff3aa13813d9fc39c9e8f2ccca67b86a7b7
jagadishe88 <jagadishe88@gmail.com>
2020-08-26 13:36:51 -0400

    Made changes to .gitignore

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
-from flask_mail import Mail, Message
+
 
 app = Flask(__name__)
-
-# set configuration and instantiate mail
-mail_settings = {
-    "MAIL_SERVER": 'smtp.gmail.com',
-    "MAIL_PORT": 465,
-    "MAIL_USE_TLS": False,
-    "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": 'jagadish.pasham@gmail.com',
-    "MAIL_PASSWORD": 'ytdzhduheugrujco'
-}
-
-app.config.update(mail_settings)
-mail = Mail(app)
 
 
 @app.route('/')
@@ -30,15 +17,15 @@ def html_page(page_name):
 app.secret_key = 'development key'
 
 
-@app.route('/home', methods=['POST', 'GET'])
-def thankyou():
-    form = request.form
-    emailfrom = form['email']
-    subject = form['subject']
-    message = form['message']
-    msg = Message(subject, sender=emailfrom, recipients=['me@jenkarla.com'])
-    mail.send(msg, message)
-    return render_template('home.html')
+# @app.route('/home', methods=['POST', 'GET'])
+# def thankyou():
+#     form = request.form
+#     emailfrom = form['email']
+#     subject = form['subject']
+#     message = form['message']
+#     msg = Message(subject, sender=emailfrom, recipients=['me@jenkarla.com'])
+#     mail.send(msg, message)
+#     return render_template('home.html')
 
 
 if __name__ == '__main__':
